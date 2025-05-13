9be42cda0250ad0c255c86b7969b175cb6db574c
1garo <alevardai427@gmail.com>
2025-05-13 19:43:49 -0300

    pivot

--- README.md
+++ README.md
@@ -1,16 +1,3 @@
 # Manta
-Your favorite task runner.
 
-## Task Runner
-Goal: Build a service that runs tasks instantaneously and concurrently.
 
-### Example
-
-```go
-```
-
-### Installation
-
-- Run `go mod init github.com/<your-user>/<your-repo>`.
-- Add the example content into `main.go` file.
-- Run `go mod tidy`.
--- main.go
+++ main.go
@@ -1,32 +1,8 @@
 package main
 
-import (
-	"log"
+import "fmt"
 
-	"github.com/1garo/manta/task"
-	"github.com/1garo/manta/runner"
-)
 
 func main() {
-	sc := runner.NewRunner()
-
-	log.Printf("runner size: %d", sc.Len())
-
-	jobName := "HelloWorld"
-	job := task.NewTask(jobName, func() error {
-		log.Println("Hello World!")
-		return nil
-	})
-
-	sc.AddTask(job)
-	log.Printf("queue length before getting job: %d", sc.Len())
-	j, ok := sc.Get(jobName)
-	log.Printf("queue length after getting job: %d", sc.Len())
-	if !ok {
-		log.Fatal("job not found")
-	}
-	err := j.Execute()
-	if err != nil {
-		log.Fatal(err)
-	}
+	fmt.Println("hello world")
 }
